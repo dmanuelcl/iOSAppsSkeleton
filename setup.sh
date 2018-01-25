dee8989f4947318e49038df633dc1e2bbb37f19c
Dani Manuel Céspedes Lara <dmanuelcl@gmail.com>
2018-01-25 17:10:05 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,16 @@
 projectName=$1
 find .  -type f -print0 | xargs -0 perl -pi -e "s/____PROJECTNAME____/$projectName/g"
 find .  -type f -print0 | xargs -0 perl -pi -e "s/----PROJECTNAME----/$projectName/g"
+
+mv "./____PROJECTNAME____/____PROJECTNAME____" "./____PROJECTNAME____/$projectName"
+mv "./____PROJECTNAME____" "./$projectName"
+mv "./____PROJECTNAME____.xcodeproj"  "./$projectName.xcodeproj"
+mv "./____PROJECTNAME____.xcworkspace"  "./$projectName.xcworkspace"
+
+mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets
+cp -R CodeSnippets/  ~/Library/Developer/Xcode/UserData/CodeSnippets
+rm -rf CodeSnippets
+rm -rf .git
 rm "setup.sh"
+git init
+pod install --verbose
